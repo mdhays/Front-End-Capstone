95501edc543395e255702ce1406a924beecb88c9
mdhays <mdhays1995@gmail.com>
2015-12-10 14:42:56 -0600

    Title screen is almost complete. Just need to crop the poke ball and adjust positioning of text and image.

--- javascripts/game.js
+++ javascripts/game.js
@@ -5,8 +5,9 @@ function preload() {
 	game.load.image('sky', './images/Hoenn.png');
 	game.load.image('pokeBall', './images/pokeball.png');
 }
-
+// Global variables
 var startButton;
+var text;
 
 
 function create() {
@@ -14,20 +15,26 @@ function create() {
 	// Loading the Hoenn background.
 	game.add.sprite(0, 0, 'sky');
 	console.log("background loads");
-	//Loads pokeball image and creates a button on it.
+	// Loads pokeball image and creates a button on it.
 	startButton = game.add.button(0, 100, 'pokeBall', whenClicked, this);
 	console.log("button loads");
 
-	var text = " STRIFE ";
+	text = " STRIFE ";
     var style = { font: "65px Rockwell", fill: "#007acc", align: "center" };
-
+    // Creates the text, positions it, and adds it to the game world.
     var t = game.add.text(game.world.centerX-300, game.world.centerY-300, text, style);
-
+    // When startButton is clicked, the 
+	function whenClicked() {
+		console.log("Click working ^^");
+		startButton.destroy();
+		console.log(text);
+		t.destroy();
+	// End of whenClicked
+	}
+// End of create
 }
 
-function whenClicked() {
-	console.log("Working ^^")
-}
+
 
 function update() {
 
