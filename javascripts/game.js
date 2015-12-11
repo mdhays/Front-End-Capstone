ffe10b913ea674dbea6e61ca56d6a1ae4b1ca46f
mdhays <mdhays1995@gmail.com>
2015-12-10 23:31:39 -0600

    Character select screen now has one character. Clicking the character name now runs a function.

--- javascripts/game.js
+++ javascripts/game.js
@@ -10,6 +10,7 @@ var startButton;
 var text;
 
 
+
 function create() {
 	
 	// Loading the Hoenn background.
@@ -19,19 +20,37 @@ function create() {
 	startButton = game.add.button(0, 100, 'pokeBall', whenClicked, this);
 	console.log("button loads");
 
-	text = " STRIFE ";
+	text = "             STRIFE ";
     var style = { font: "65px Rockwell", fill: "#007acc", align: "center" };
     // Creates the text, positions it, and adds it to the game world.
     var t = game.add.text(game.world.centerX-300, game.world.centerY-300, text, style);
-    
 
+    var RD;
+    
     // When startButton is clicked, this function executes.
 	function whenClicked() {
+		// Button and text are removed...
 		console.log("Click working ^^");
 		startButton.destroy();
 		t.destroy();
+
+		// and new text and buttons, are created.
 		text = " Choose Your Character ";
 		t = game.add.text(game.world.centerX-300, game.world.centerY-300, text, style);
+		RD = " Rainbow Dash ";
+		
+
+		rainbowDash = game.add.text(game.world.centerX-300, game.world.centerY-100, RD, style);
+		rainbowDash.events.onInputDown.add(selected, this);
+		rainbowDash.inputEnabled = true;
+		
+		function selected() {
+			
+			rainbowDash.destroy();
+			t.destroy();
+		}
+
+
 	// End of whenClicked
 	}
 // End of create
