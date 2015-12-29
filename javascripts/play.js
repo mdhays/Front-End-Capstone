3e2f66c699914f187b43337f577bf405bf2257fb
mdhays <mdhays1995@gmail.com>
2015-12-29 15:06:49 -0600

    Character and floor now collide. Character is now able to jump on contact with the ground.

--- javascripts/play.js
+++ javascripts/play.js
@@ -4,12 +4,15 @@ var player;
 var gravestone;
 var graveYard;
 var skeleton;
+var cursors;
+var ground;
 
 function preload() {
     
     game.load.image('graveyard', './sprites/cemetary.png');
     game.load.image('gravestone', './sprites/gravestone.png');
-    game.load.spritesheet('skeleton', './sprites/skeleton.png', 75, 95, 21);
+    game.load.image('ground', './sprites/blacksquare.jpg')
+    game.load.spritesheet('skeleton', './sprites/skeleton.png', 73, 89, 21);
 
 }
 
@@ -29,12 +32,26 @@ function create() {
 
     gravestone.scale.setTo(.1);
 
-    skeleton = game.add.sprite(30, game.world.height - 150, 'skeleton');
+    skeleton = game.add.sprite(30, game.world.height - 260, 'skeleton');
 
-    skeleton.animations.add('right', [1 ,2, 4, 5], 7, true);
+    ground = game.add.sprite(30, game.world.height - 120, 'ground');
 
-    //  We need to enable physics on the player
+    ground.scale.setTo(.1);
+
+    skeleton.animations.add('right', [1 ,2, 4, 5], 10, true);
+
+    // enable physics on the player
     game.physics.enable(skeleton, Phaser.Physics.ARCADE);
+
+    // enable physics on the black square serving as the ground.
+    game.physics.enable(ground, Phaser.Physics.ARCADE);
+
+    ground.body.immovable = true;
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    skeleton.body.gravity.y = 400;
+
 
 
 }
@@ -42,9 +59,20 @@ function create() {
 
 function update() {
 
+
+    // Checks for collision of the skeleton and floor.
+    game.physics.arcade.collide(skeleton, ground);
+
     graveYard.tilePosition.x -= 2;
 
     skeleton.animations.play('right');
+
+     //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && skeleton.body.touching.down)
+    {
+        skeleton.body.velocity.y = -300;
+    }
+
 
 }
 
@@ -53,5 +81,6 @@ function update() {
 function render() {
 
     // game.debug.body(skeleton);
+    // game.debug.body(ground);
 
 }
