c90422ffea0d7699bf81ef5672fa17713c17ff9b
mdhays <mdhays1995@gmail.com>
2015-12-29 13:49:40 -0600

    Physics on the skeleton is now enabled.

--- javascripts/play.js
+++ javascripts/play.js
@@ -3,17 +3,21 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 var player;
 var gravestone;
 var graveYard;
+var skeleton;
 
 function preload() {
     
     game.load.image('graveyard', './sprites/cemetary.png');
     game.load.image('gravestone', './sprites/gravestone.png');
-    game.load.spritesheet('skeleton', './sprites/skeleton.png', 80, 100, 50);
+    game.load.spritesheet('skeleton', './sprites/skeleton.png', 74, 95, 50);
 
 }
 
 
 function create() {
+
+        // Assigning Fps
+    game.time.desiredFps = 60;
 
     // Here we set the color of the background. I made it white since the image is black.
     game.stage.backgroundColor = 0xffffff;
@@ -25,7 +29,12 @@ function create() {
 
     gravestone.scale.setTo(.1);
 
-    game.add.sprite(0, game.world.height - 150, 'skeleton');
+    skeleton = game.add.sprite(30, game.world.height - 150, 'skeleton');
+
+    skeleton.animations.add('right', [3, 4, 5, 6, 7], true);
+
+    //  We need to enable physics on the player
+    game.physics.enable(skeleton, Phaser.Physics.ARCADE);
 
 
 }
@@ -35,10 +44,14 @@ function update() {
 
     graveYard.tilePosition.x -= 2;
 
+    skeleton.animations.play('right');
+
 }
 
 
 
 function render() {
+
+    game.debug.body(skeleton);
 
 }
